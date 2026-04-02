d8d839afe74102024698f53d042eff837308974b
SefoTud <54956059+SefoTud@users.noreply.github.com>
2026-04-02 18:35:05 +0200

    Refactor fetch event listener in sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,1 @@
-const CACHE_NAME = 'precio-combustibles-v2';
-
-self.addEventListener('install', (event) => {
-    self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-    event.waitUntil(clients.claim());
-});
-
-self.addEventListener('fetch', (event) => {
-    // Respondemos a las peticiones para que el navegador nos valide como PWA
-    event.respondWith(
-        fetch(event.request).catch(() => {
-            return new Response("App offline");
-        })
-    );
-});
+self.addEventListener('fetch', function(event) {});
