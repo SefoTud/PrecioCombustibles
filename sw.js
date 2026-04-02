94dd912bd900dd4ae199ff31650540e4003003f6
SefoTud <54956059+SefoTud@users.noreply.github.com>
2026-04-02 18:06:05 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
-self.addEventListener('fetch', function(event) {});
+const CACHE_NAME = 'gasofa-pro-v1';
+
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+    // No hacemos nada, dejamos que la app funcione siempre online
+});
